b63c85e3b17ce9df09d61f0f866a454a8d1363ba
Hussain Nagaria <hussainbhaitech@gmail.com>
2025-10-09 19:02:30 +0530

    feat: web form for talk CFP

--- buzz/proposals/doctype/talk_proposal/talk_proposal.py
+++ buzz/proposals/doctype/talk_proposal/talk_proposal.py
@@ -1,7 +1,7 @@
 # Copyright (c) 2025, BWH Studios and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
 
 
@@ -14,14 +14,16 @@ class TalkProposal(Document):
 	if TYPE_CHECKING:
 		from frappe.types import DF
 
-		from buzz.events.doctype.proposal_speaker.proposal_speaker import ProposalSpeaker
+		from buzz.proposals.doctype.proposal_speaker.proposal_speaker import ProposalSpeaker
 
 		description: DF.TextEditor | None
 		event: DF.Link
 		speakers: DF.Table[ProposalSpeaker]
 		status: DF.Literal["Review Pending", "Shortlisted", "Approved", "Rejected"]
-		submitted_by: DF.Link
+		submitted_by: DF.Link | None
 		title: DF.Data
 	# end: auto-generated types
 
-	pass
+	def validate(self):
+		if not self.submitted_by:
+			self.submitted_by = frappe.session.user
