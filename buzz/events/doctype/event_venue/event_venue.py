b8b4536077acc0e83602b7e3b6b2706f29fe2f22
Hussain Nagaria <hussainbhaitech@gmail.com>
2025-10-09 10:11:15 +0530

    fix: use code field instead of html editor

--- buzz/events/doctype/event_venue/event_venue.py
+++ buzz/events/doctype/event_venue/event_venue.py
@@ -15,7 +15,7 @@ class EventVenue(Document):
 		from frappe.types import DF
 
 		address: DF.SmallText
-		google_maps_embed_code: DF.HTMLEditor | None
+		google_maps_embed_code: DF.Code | None
 		latitude: DF.Float
 		longitude: DF.Float
 		type: DF.Literal["Embed Google Maps", "Open Street Map"]
